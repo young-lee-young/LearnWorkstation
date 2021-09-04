6c046698df0c2e3e7e87016c379fdf31c49610b7
leeyoung <yaoli.li@ucloud.cn>
2021-09-04 10:05:58 +0800

    company mac commit

--- Code/test.py
+++ Code/test.py
@@ -1,7 +1,13 @@
-from queue import PriorityQueue
+import requests
 
-pq = PriorityQueue()
+data = {
+    "request_id": "ccb515a7-0925-4ddf-94df-f2ce13003663",
+    "phone_id": "uphone-lee",
+    "candidates": ["1", "2"]
+}
 
-pq.put((1, "a"))
-pq.put((2, "b"))
-pq.put((3, "c"))
+cert = ("/Users/leeyoung/WorkStation/Code/signal-server/tls/server.crt", "/Users/leeyoung/WorkStation/Code/signal-server/tls/server.key")
+verify = "/Users/leeyoung/WorkStation/Code/signal-server/tls/server.crt"
+
+response = requests.post("https://127.0.0.1:443/api/v1/send_candidate", data=data, cert=cert)
+print response
--- Learn/Go/learn/go.mod
+++ Learn/Go/learn/go.mod
@@ -1,3 +1,5 @@
 module learn
 
 go 1.13
+
+require github.com/jinzhu/configor v1.2.1
