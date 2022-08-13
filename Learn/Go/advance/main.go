06808840ff4ac82ac1bf63255f74b9e67f3b8f87
leeyoung <yaoli.li@ucloud.cn>
2022-08-13 12:39:41 +0800

    company mac commit

--- Learn/Go/advance/main.go
+++ Learn/Go/advance/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import "sync"
 
 func main() {
-	i := 0
-	fmt.Println(i)
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(wg *sync.WaitGroup) {
+			var counter int
+			for i := 0; i < 1e10; i++ {
+				counter++
+			}
+			wg.Done()
+		}(&wg)
+	}
 }
